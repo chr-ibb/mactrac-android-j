9772dc1f4a7785a7569b664b81b7eeb710878eec
chr-ibb L <46900764+chr-ibb@users.noreply.github.com>
2020-05-09 03:44:59 -0700

    Added a bunch of comments/doc strings. Reorganized code in DayScreenSlideFragment.

--- mactrac/app/src/main/java/chribb/mactrac/ui/day/DayScreenSlideFragment.java
+++ mactrac/app/src/main/java/chribb/mactrac/ui/day/DayScreenSlideFragment.java
@@ -28,11 +28,11 @@ import chribb.mactrac.R;
 public class DayScreenSlideFragment extends Fragment {
     private DayViewModel dayViewModel;
     private AppBarViewModel appBarViewModel;
+    private RecyclerView recyclerView;
     private FoodListAdapter adapter;
     private int daysSinceEpoch;
-    private ItemTouchHelper helper;
+    private ItemTouchHelper itemTouchHelper;
     private boolean isEditMode;
-
     private View thisView;
 
     private Macro deleteMacro;
@@ -61,13 +61,131 @@ public class DayScreenSlideFragment extends Fragment {
         assert getArguments() != null;
         daysSinceEpoch = getArguments().getInt("daysSinceEpoch", 0);
 
+        itemTouchHelper = makeItemTouchHelper();
 
-        //TODO can i put this somewhere else?
-        helper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP
+        return inflater.inflate(
+                R.layout.fragment_screen_slide_day, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        thisView = view;
+        recyclerView = view.findViewById(R.id.recyclerview);
+        adapter = new FoodListAdapter(getContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        itemTouchHelper.attachToRecyclerView(recyclerView);
+
+        editModeTextView = view.findViewById(R.id.edit_mode_text);
+        //TODO consider view binding or data binding
+        TextView dateText = view.findViewById(R.id.date_text);
+        TextView relativeDayText = view.findViewById(R.id.relative_day_text);
+        TextView totalCaloriesText = view.findViewById(R.id.total_calories_text);
+        TextView totalProteinText = view.findViewById(R.id.total_protein_text);
+        TextView totalFatText = view.findViewById(R.id.total_fat_text);
+        TextView totalCarbsText = view.findViewById(R.id.total_carbs_text);
+
+        dateText.setText(dayViewModel.getDateText(daysSinceEpoch));
+        relativeDayText.setText(dayViewModel.getRelativeDayText(daysSinceEpoch));
+
+        disableEditMode();
+
+        /* AppBar button observer for toggling edit mode */
+        appBarViewModel.getEditPressed().observe(getViewLifecycleOwner(), new Observer<Boolean>() {
+            @Override
+            public void onChanged(@NonNull final Boolean pressed) {
+                if (pressed && dayViewModel.getDayOnScreen() == daysSinceEpoch) {
+                    // TODO there is a bug when pressing edit button after adding where it doesn't
+                    // toggle edit mode for the correct day, presumably because you're observing
+                    // this on all the DayScreenSlideFragments that are active instead of just one
+                    toggleEditMode();
+                    appBarViewModel.setEditPressed(false);
+                }
+            }
+        });
+
+        /* Observer for the list of Macros that belong to this day */
+        dayViewModel.loadFood(daysSinceEpoch).observe(getViewLifecycleOwner(), new Observer<List<Macro>>() {
+
+            /**
+             * Submits MACROS to adapter, visually updating screen if the list 'looks' different.
+             * Calculates totals for each macro type and updates textViews with the results.
+             *
+             * @param macros List of macros for this day from Room.
+             */
+            @Override
+            public void onChanged(@Nullable final List<Macro> macros) {
+
+                adapter.submitList(macros);
+
+                //TODO move as much out/into view model as possible
+                int calories = 0;
+                int protein = 0;
+                int fat = 0;
+                int carbs = 0;
+
+                for (int i = 0; i < macros.size(); i++) {
+                    Macro current = macros.get(i);
+                    calories += current.getCalories();
+                    protein += current.getProtein();
+                    fat += current.getFat();
+                    carbs += current.getCarbs();
+                }
+                String totalCalories = calories + " Calories";
+                totalCaloriesText.setText(totalCalories);
+                String totalProtein = protein + "g Protein";
+                totalProteinText.setText(totalProtein);
+                String totalFat = fat + "g Fat";
+                totalFatText.setText(totalFat);
+                String totalCarbs = carbs + "g Carbs";
+                totalCarbsText.setText(totalCarbs);
+            }
+        });
+    }
+
+    /**
+     * Called when Fragment is paused, more or less when the fragment leaves the screen.
+     * Disables edit mode if it is enabled.
+     */
+    @Override
+    public void onPause() {
+        super.onPause();
+        disableEditMode();
+    }
+
+
+    /* Private Methods */
+
+
+    /**
+     * Creates the appropriate ItemTouchHelper for the recycler view and returns it.
+     * Solely exists to move the code out of onCreateView
+     * @return a new ItemTouchHelper to attach to the recycler view
+     */
+    private ItemTouchHelper makeItemTouchHelper() {
+        return new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP
                 | ItemTouchHelper.DOWN, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
 
-
-
+            /**
+             * Called when an item in recycler view is moved (up or down, after long touch).
+             * "moved" does not mean dragged and dropped, it means dragged over a different item.
+             * This can be called multiple times before the item is dropped.
+             * It's my understanding that while the 'to' and 'from' positions are usually adjacent,
+             * they can be further apart with quick drags.
+             *
+             * Creates a copy of the list of macros in the adapter for editing. Shifts over every
+             * item between the 'from' and 'to' positions by swapping with each item on the way,
+             * inserting the item where it was dragged to. Updates the POSITION of each macro while
+             * shifting. Updates the list of macros in adapter with the changed copy.
+             *
+             * DOES NOT update the Room Database. This is done when the item is finally dropped.
+             * @param recyclerView Recycler view where the 'move' is taking place.
+             * @param viewHolder ViewHolder holding the view/item that is being moved.
+             * @param target ViewHolder holding the view/item that is being moved to.
+             * @return Boolean, whether or not the item was moved.
+             */
             @Override
             public boolean onMove(@NonNull RecyclerView recyclerView,
                                   @NonNull RecyclerView.ViewHolder viewHolder,
@@ -99,16 +217,30 @@ public class DayScreenSlideFragment extends Fragment {
                     }
                 }
                 adapter.submitList(macrosCopy);
-//                adapter.notifyItemMoved(fromPosition, toPosition);
                 return true;
             }
 
+            /**
+             * Called when an item in the recycler view is dropped from being dragged (moved).
+             * Now that the Macro positions are finished updating from the move, update Room.
+             * @param recyclerView RecyclerView in which item was dragged and dropped.
+             * @param viewHolder ViewHolder holding the view that was dragged and dropped.
+             */
             @Override
             public void clearView(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder) {
                 super.clearView(recyclerView, viewHolder);
                 dayViewModel.update(adapter.getCurrentList());
             }
 
+            /**
+             * Called when an item in the recycler view is swiped away (left or right).
+             * Creates a copy of the macro list, deletes the macro from room (which updates the
+             * visible list on screen automatically), corrects the POSITION of each macro after
+             * the deleted one, and updates Room with the new position values.
+             * Shows snackbar for undoing delete.
+             * @param viewHolder ViewHolder in which item was swiped.
+             * @param direction Direction of the swipe (irrelevant, both directions work the same).
+             */
             @Override
             public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
                 List<Macro> macrosCopy = new ArrayList<>(adapter.getCurrentList());
@@ -125,11 +257,22 @@ public class DayScreenSlideFragment extends Fragment {
                 showUndoSnackbar();
             }
 
+            /**
+             * isEditMode gets toggled whenever the edit button is pressed, and gets set to false
+             * when the fragment is paused.
+             * TODO update these two comments when the method for toggling edit mode is changed
+             * @return whether or not swiping items in recycler view is currently enabled
+             */
             @Override
             public boolean isItemViewSwipeEnabled() {
                 return isEditMode;
             }
 
+            /**
+             * isEditMode gets toggled whenever the edit button is pressed, and gets set to false
+             * when the fragment is paused.
+             * @return whether or not long press dragging in recycler view is currently enabled
+             */
             @Override
             public boolean isLongPressDragEnabled() {
                 return isEditMode;
@@ -137,89 +280,11 @@ public class DayScreenSlideFragment extends Fragment {
 
 
         });
-
-        return inflater.inflate(
-                R.layout.fragment_screen_slide_day, container, false);
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        thisView = view;
-
-        appBarViewModel.getEditPressed().observe(getViewLifecycleOwner(), new Observer<Boolean>() {
-            @Override
-            public void onChanged(@NonNull final Boolean pressed) {
-                if (pressed && dayViewModel.getDayOnScreen() == daysSinceEpoch) {
-                    // TODO there is a bug when pressing edit button after adding where it doesn't
-                    // toggle edit mode for the correct day, presumably because you're observing
-                    // this on all the dayscreenslidefragments that are active instead of just one
-                    toggleEditMode();
-                    appBarViewModel.setEditPressed(false);
-                }
-            }
-        });
-
-        //TODO consider view binding or data binding
-        TextView dateText = view.findViewById(R.id.date_text);
-        TextView relativeDayText = view.findViewById(R.id.relative_day_text);
-        TextView totalCaloriesText = view.findViewById(R.id.total_calories_text);
-        TextView totalProteinText = view.findViewById(R.id.total_protein_text);
-        TextView totalFatText = view.findViewById(R.id.total_fat_text);
-        TextView totalCarbsText = view.findViewById(R.id.total_carbs_text);
-
-        editModeTextView = view.findViewById(R.id.edit_mode_text);
-
-        RecyclerView recyclerView = view.findViewById(R.id.recyclerview);
-        adapter = new FoodListAdapter(getContext());
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        helper.attachToRecyclerView(recyclerView);
-
-        dayViewModel.loadFood(daysSinceEpoch).observe(getViewLifecycleOwner(), new Observer<List<Macro>>() {
-            @Override
-            public void onChanged(@Nullable final List<Macro> macros) {
-
-                // Update the List of macros in ListAdapter
-                adapter.submitList(macros);
-
-                int calories = 0;
-                int protein = 0;
-                int fat = 0;
-                int carbs = 0;
-
-                for (int i = 0; i < macros.size(); i++) {
-                    Macro current = macros.get(i);
-                    calories += current.getCalories();
-                    protein += current.getProtein();
-                    fat += current.getFat();
-                    carbs += current.getCarbs();
-                }
-                String totalCalories = calories + " Calories";
-                totalCaloriesText.setText(totalCalories);
-                String totalProtein = protein + "g Protein";
-                totalProteinText.setText(totalProtein);
-                String totalFat = fat + "g Fat";
-                totalFatText.setText(totalFat);
-                String totalCarbs = carbs + "g Carbs";
-                totalCarbsText.setText(totalCarbs);
-            }
-        });
-
-        dateText.setText(dayViewModel.getDateText(daysSinceEpoch));
-        relativeDayText.setText(dayViewModel.getRelativeDayText(daysSinceEpoch));
-
-        disableEditMode();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        //disable edit mode when you leave this 'day'
-        disableEditMode();
-    }
-
+    /**
+     * Toggles edit mode.
+     */
     private void toggleEditMode() {
         if (isEditMode) {
             disableEditMode();
@@ -227,19 +292,34 @@ public class DayScreenSlideFragment extends Fragment {
             enableEditMode();
         }
     }
+
+    /**
+     * Enables edit mode.
+     */
     private void enableEditMode() {
         //TODO  Make a textview visable that says "swipe to delete, click to edit"
-        // change layout to like highlight the section to be edited. make it noticably different
-        // maybe increase the "height" of each recycler item, like its floating higher now (shadow)
+        // change layout to like highlight the section to be edited. Make it noticeably different.
+        // Maybe increase the "height" of each recycler item, like its floating higher now (shadow)
         editModeTextView.setVisibility(View.VISIBLE);
+
         isEditMode = true;
     }
+
+    /**
+     * Disables edit mode
+     */
     private void disableEditMode() {
         //TODO get rid of textView, change layout back
         editModeTextView.setVisibility(View.GONE);
         isEditMode = false;
     }
 
+    /**
+     * Handles showing a Snackbar when a Macro is swipe deleted, allowing the option to Undo
+     * the deletion by clicking the undo button.
+     * In English the String displayed will be "Deleted [food]",
+     * and the undo button will read "Click to Undo"
+     */
     private void showUndoSnackbar() {
         //TODO extract strings
         String snackString = "Deleted " + deleteMacro.getFood();
@@ -247,6 +327,10 @@ public class DayScreenSlideFragment extends Fragment {
         snackbar.setAction("Click to Undo", f -> undoDelete());
         snackbar.show();
     }
+
+    /**
+     * Undoes the most previous deletion. Updates the POSITION of each macro below the insertion.
+     */
     private void undoDelete() {
         List<Macro> macrosCopy = new ArrayList<>(adapter.getCurrentList());
         for (int i = deletePosition; i < macrosCopy.size(); i++) {
@@ -254,5 +338,16 @@ public class DayScreenSlideFragment extends Fragment {
         }
         dayViewModel.update(macrosCopy);
         dayViewModel.insert(deleteMacro);
+    }
+
+    /**
+     * For testing. Shows a snackbar when DayScreenSlideFragment is paused, displaying which day's
+     * fragment was paused.
+     */
+    private void showPausedSnackbar() {
+        String dayString = dayViewModel.getDateText(daysSinceEpoch);
+        String snackString = dayString + " PAUSED";
+        Snackbar snackbar = Snackbar.make(thisView, snackString, Snackbar.LENGTH_SHORT);
+        snackbar.show();
     }
 }
